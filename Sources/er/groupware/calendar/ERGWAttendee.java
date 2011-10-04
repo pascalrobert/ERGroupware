0802a355b1599d4f8f1f9a755dae521922530c05
Pascal Robert <probert@macti.ca>
2011-10-04 19:43:59 -0400

    Moved role to contact.
    Added support for the X-SENT parameter, so that the server won't send
    invitations if you are importing events to a CalDAV server.

--- Sources/er/groupware/calendar/ERGWAttendee.java
+++ Sources/er/groupware/calendar/ERGWAttendee.java
@@ -7,15 +7,14 @@ import er.groupware.calendar.enums.ERGWParticipantStatus;
 
 public class ERGWAttendee extends ERGWContact {
 
-  private ERGWAttendeeRole role;
   private ERGWParticipantStatus partStat;
   private boolean rsvp;
   private ERGWCUType cutype;
   private String memberOf; // http://tools.ietf.org/html/rfc2445#section-4.2.11
   private ERGWContact delegatedFrom;
   private ERGWContact delegatedTo;
+  private boolean rsvpSent;
   
-  public static final ERXKey<ERGWAttendeeRole> ROLE = new ERXKey<ERGWAttendeeRole>("role");
   public static final ERXKey<ERGWParticipantStatus> PART_STAT = new ERXKey<ERGWParticipantStatus>("partStat");
   public static final ERXKey<Boolean> RSVP = new ERXKey<Boolean>("rsvp");
   public static final ERXKey<ERGWCUType> CUTYPE = new ERXKey<ERGWCUType>("cutype");
@@ -27,60 +26,60 @@ public class ERGWAttendee extends ERGWContact {
     
   }
 
-  public ERGWAttendeeRole role() {
-    return role;
-  }
-
-  public void setRole(ERGWAttendeeRole role) {
-    this.role = role;
-  }
-
   public ERGWParticipantStatus partStat() {
     return partStat;
   }
 
-  public void setPartStat(ERGWParticipantStatus partStat) {
-    this.partStat = partStat;
+  public void setPartStat(ERGWParticipantStatus _partStat) {
+    this.partStat = _partStat;
   }
 
   public boolean isRsvp() {
     return rsvp;
   }
 
-  public void setRsvp(boolean rsvp) {
-    this.rsvp = rsvp;
+  public void setRsvp(boolean _rsvp) {
+    this.rsvp = _rsvp;
   }
 
   public ERGWCUType cutype() {
     return cutype;
   }
 
-  public void setCutype(ERGWCUType cutype) {
-    this.cutype = cutype;
+  public void setCutype(ERGWCUType _cutype) {
+    this.cutype = _cutype;
   }
 
   public String memberOf() {
     return memberOf;
   }
 
-  public void setMemberOf(String memberOf) {
-    this.memberOf = memberOf;
+  public void setMemberOf(String _memberOf) {
+    this.memberOf = _memberOf;
   }
 
   public ERGWContact delegatedFrom() {
     return delegatedFrom;
   }
 
-  public void setDelegatedFrom(ERGWContact delegatedFrom) {
-    this.delegatedFrom = delegatedFrom;
+  public void setDelegatedFrom(ERGWContact _delegatedFrom) {
+    this.delegatedFrom = _delegatedFrom;
   }
 
   public ERGWContact delegatedTo() {
     return delegatedTo;
   }
 
-  public void setDelegatedTo(ERGWContact delegatedTo) {
-    this.delegatedTo = delegatedTo;
+  public void setDelegatedTo(ERGWContact _delegatedTo) {
+    this.delegatedTo = _delegatedTo;
+  }
+  
+  public boolean isRsvpSent() {
+	  return rsvpSent;
+  }
+
+  public void setRsvpSent(boolean _rsvpSent) {
+	  this.rsvpSent = _rsvpSent;
   }
     
 }
--- Sources/er/groupware/calendar/ERGWContact.java
+++ Sources/er/groupware/calendar/ERGWContact.java
@@ -1,6 +1,7 @@
 package er.groupware.calendar;
 
 import er.extensions.eof.ERXKey;
+import er.groupware.calendar.enums.ERGWAttendeeRole;
 
 public abstract class ERGWContact {
 
@@ -10,60 +11,70 @@ public abstract class ERGWContact {
   private String sentBy;
   private String ldapUrl;
   private String language;
-  
+  private ERGWAttendeeRole role;
+
   public static final ERXKey<String> EMAIL_ADDRESS = new ERXKey<String>("emailAddress");
   public static final ERXKey<String> URL = new ERXKey<String>("url");
   public static final ERXKey<String> NAME = new ERXKey<String>("name");
   public static final ERXKey<String> SENT_BY = new ERXKey<String>("sentBy");
   public static final ERXKey<String> LDAP_URL = new ERXKey<String>("ldapUrl");
   public static final ERXKey<String> LANGUAGE = new ERXKey<String>("language");
-  
+  public static final ERXKey<ERGWAttendeeRole> ROLE = new ERXKey<ERGWAttendeeRole>("role");
+
   public String emailAddress() {
     return emailAddress;
   }
   
-  public void setEmailAddress(String emailAddress) {
-    this.emailAddress = emailAddress;
+  public void setEmailAddress(String _emailAddress) {
+    this.emailAddress = _emailAddress;
   }
   
   public String url() {
     return url;
   }
   
-  public void setUrl(String url) {
-    this.url = url;
+  public void setUrl(String _url) {
+    this.url = _url;
   }
   
   public String name() {
     return name;
   }
   
-  public void setName(String name) {
-    this.name = name;
+  public void setName(String _name) {
+    this.name = _name;
   }
   
   public String sentBy() {
     return sentBy;
   }
   
-  public void setSentBy(String sentBy) {
-    this.sentBy = sentBy;
+  public void setSentBy(String _sentBy) {
+    this.sentBy = _sentBy;
   }
   
   public String ldapUrl() {
     return ldapUrl;
   }
   
-  public void setLdapUrl(String ldapUrl) {
-    this.ldapUrl = ldapUrl;
+  public void setLdapUrl(String _ldapUrl) {
+    this.ldapUrl = _ldapUrl;
   }
   
   public String language() {
     return language;
   }
   
-  public void setLanguage(String language) {
-    this.language = language;
+  public void setLanguage(String _language) {
+    this.language = _language;
   }
    
+  public ERGWAttendeeRole role() {
+    return role;
+  }
+
+  public void setRole(ERGWAttendeeRole _role) {
+    this.role = _role;
+  }
+
 }
